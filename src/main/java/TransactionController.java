518743b0d3a71797a36770bf320b51cfec32a008
Martin Grigorov <maptuhec@gmail.com>
2018-03-17 16:02:01 +0200

    Fix the params for the rest call

--- src/main/java/TransactionController.java
+++ src/main/java/TransactionController.java
@@ -1,3 +1,5 @@
+package hello;
+
 import org.springframework.web.bind.annotation.RestController;
 import org.web3j.crypto.Credentials;
 import org.web3j.crypto.WalletUtils;
@@ -16,14 +18,19 @@ import java.io.FileWriter;
 import java.math.BigDecimal;
 import java.math.BigInteger;
 
-import org.json.JSONObject;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
+
+
 @RestController
 @RequestMapping(method = RequestMethod.POST)
-public class TransactionController {
+public class HelloController {
+	
 
 	public TransactionReceipt sendFundsToAccount(String amount, String recipient)
 			throws InterruptedException, TransactionException, Exception {
@@ -36,14 +43,14 @@ public class TransactionController {
 
 		// This JSON String should be stored in the backend an we should read it from
 		// there, also the password
-		String jsonString = "{ \"version\": 3, \"id\": \"1ddf0087-79e0-48d9-a84b-5d364075a804\", \"address\": \"627306090abab3a6e1400e9345bc60c78a8bef57\", \"Crypto\": { \"ciphertext\": \"6d845d338f2cfc526adc5d49b5f95c13690b59ef56c9eb2c79e4ff7740adcc77\", \"cipherparams\": { \"iv\": \"f506358d43a8cd8bea70b34b3bd97190\" }, \"cipher\": \"aes-128-ctr\", \"kdf\": \"scrypt\", \"kdfparams\": { \"dklen\": 32, \"salt\": \"e15ccf22e4e35a5a321885c562e265d9fd7d0fbede7894e9195108bbf40563a2\", \"n\": 1024, \"r\": 8, \"p\": 1 }, \"mac\": \"2295347e6e31672df9e9e651f6d6593d769c848e9ceb2b15dd5e09fd2aaa4992\" } }";
+		String jsonString = "{\"version\":3,\"id\":\"d44f162d-1f91-4ade-9d5f-414661295df0\",\"address\":\"b63df2068d209f8ff3925c4c9dbbabfd31301825\",\"Crypto\":{\"ciphertext\":\"2d40317fc74b4ea71930a0a6681507addc103e127e65a663cf731537ef79726d\",\"cipherparams\":{\"iv\":\"f90df4df3a67d85e7e34f84a7c0b15fd\"},\"cipher\":\"aes-128-ctr\",\"kdf\":\"scrypt\",\"kdfparams\":{\"dklen\":32,\"salt\":\"473b462c67127e8260fea0ff7fe716d17b6792278160937a29d8875294d0f92a\",\"n\":8192,\"r\":8,\"p\":1},\"mac\":\"b94eada113e11314895cb559bd79e72c0dc27eb15ed4ebb666ffb80977859f13\"}}";
 		String password = "123456789";
 
 		// TODO: Need to make a config file from which we should get information about
 		// the environment and set it to the server
-		Web3j web3 = Web3j.build(new HttpService("https://ropsten.infura.io/Up5uvBHSCSqtOmnlhL87")); // defaults to
-																										// http://localhost:8545/
-
+//		Web3j web3 = Web3j.build(new HttpService()); // defaults to
+	 Web3j web3 = Web3j.build(new HttpService("https://ropsten.infura.io/Up5uvBHSCSqtOmnlhL87"));																								// http://localhost:8545/
+		
 		// Create temp file from the json string to create the credentialls
 		File tempWalletFile = File.createTempFile("temp-wallet-file", ".tmp");
 		BufferedWriter writer = new BufferedWriter(new FileWriter(tempWalletFile));
@@ -58,15 +65,13 @@ public class TransactionController {
 		EthGetBalance ethGetBalance = web3.ethGetBalance(recipient, DefaultBlockParameterName.LATEST).sendAsync().get();
 
 		BigInteger wei = ethGetBalance.getBalance();
-		System.out.println(wei);
 		return transactionReceipt;
 	}
-
-	@RequestMapping("/sendFundsToAccount")
-	public TransactionReceipt sendEther(@RequestParam(value = "amount") String amount,
-			@RequestParam(value = "recipient") String recipient)
+	@CrossOrigin(origins = "*")
+	@RequestMapping(value = "/sendFundsToAccount", method = RequestMethod.POST)
+	public TransactionReceipt sendEther(@RequestBody FundTransactionParams fundParams)
 			throws InterruptedException, TransactionException, Exception {
-		return sendFundsToAccount(amount, recipient);
+		return sendFundsToAccount(fundParams.amount, fundParams.recipient);
 
 	}
 
